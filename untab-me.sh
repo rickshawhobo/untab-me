586d795191d983ceba1e21422fdfc4f7251cdd57
Guy Huynh <ghuynh@healthrecoverysolutions.com>
2018-04-18 22:52:06 -0400

    actually making it work

--- untab-me.sh
+++ untab-me.sh
@@ -4,7 +4,7 @@
 
 if [ -z "$1" ]
 	then
-		echo "Usage example: untab-me.sh *.php to replace tabs with 4 spaces from all files ending in .php"
+		echo "Usage example: untab-me.sh php to replace tabs with 4 spaces from all files ending in .php"
 else
-	find . -name "${1}" ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
+	find . -name "*.${1}" ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
 fi
